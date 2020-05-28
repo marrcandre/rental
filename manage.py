5ebdb981d6be73b0c4f85fc55e317d5eafa75634
Marco André Mendes <marcoandre@gmail.com>
2020-05-27 22:12:20 -0300

    Projeto recriado em nova estrutura

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "restonomicon.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rental.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
