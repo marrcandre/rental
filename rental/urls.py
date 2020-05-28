0b4116bfd715dd0597e60e66174af1eb1b29fa26
Marco André Mendes <marcoandre@gmail.com>
2020-05-28 15:06:46 -0300

    Login e autenticação configurados

--- rental/urls.py
+++ rental/urls.py
@@ -6,4 +6,9 @@ from .api import router
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include(router.urls)),
+    path("api/auth/", include("djoser.urls.authtoken")),
 ]
+# comando para obter credenciais de autenticação:
+# curl -X POST -d '{"username": "admin","password": "deco1234"}' \
+# -H 'Content-Type: application/json' \
+# http://127.0.0.1:8000/api/auth/token/login/
